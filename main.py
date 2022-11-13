9889e31c6439fd855744af41f16933760579c3c0
cipherdot <r.m.kavinbharathi@gmail.com>
2022-11-13 13:38:25 +0530

    prod: rich texts

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 
+# ------------------------------------------------------------------------------------------------------------------- #
 
 import sys
 import os
+import rich
+
+# ------------------------------------------------------------------------------------------------------------------- #
 
 project_name = sys.argv[1]
-print(f"Creating {project_name}...")
+rich.print(f"[blue]Creating[/blue]: [bold green]{project_name}[/bold green]")
 os.chdir(os.getcwd())
 
 try:
     os.mkdir(project_name)
 except OSError:
-    print(f"Project: {project_name} already exists")
+    rich.print(f"[bold red]Error[/bold red]: [bold blue]Project {project_name} already exists[/bold blue]")
     exit()
 
 # ------------------------------------------------- create html file ------------------------------------------------- #
@@ -24,17 +28,19 @@ with open(f'{project_name}/index.html', 'w') as html:
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>{project_name}</title>
+    <title>{project_name.capitalize()}</title>
 </head>
 <body>
     <div id="dev">
-        <h1>{project_name} created successfully!</h1>
+        <h1>{project_name.capitalize()} created successfully!</h1>
     </div>
 </body>
 </html>
 '''
     )
     html.close()
+
+rich.print(f"[blue]Created[/blue]: [bold red]index.html[/bold red]")
 
 # ------------------------------------------------- create css file ------------------------------------------------- #
 with open(f'{project_name}/style.css', 'w') as css:
@@ -70,14 +76,19 @@ main {
     )
     css.close()
 
+rich.print(f"[blue]Created[/blue]: [bold blue]style.css[/bold blue]")
+
 # ------------------------------------------------- create js file ------------------------------------------------- #
 with open(f'{project_name}/app.js', 'w') as js:
     js.write(
 f'''
-console.log('{project_name} successfully!')
+console.log('{project_name.capitalize()} successfully!')
+const dev = document.getElementById('dev')
 '''
     )
     js.close()
 
+rich.print(f"[blue]Created[/blue]: [bold yellow]app.js[/bold yellow]")
 
-print(f"Completed {project_name}")
+rich.print("[bold green]Completed...[/bold green]")
+# ------------------------------------------------------------------------------------------------------------------- #
